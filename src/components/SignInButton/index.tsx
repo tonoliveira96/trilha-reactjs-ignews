efc41f5272fca29004d1f82dc080c0890549667a
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-11-06 15:06:51 -0300

    Configurado auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,7 +7,11 @@ export function SignInButton() {
   const [session] = useSession();
 
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={()=> signOut()}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
       {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
